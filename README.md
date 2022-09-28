425f4262959956f7f44a595fc89838d77e54aa87
Naokitsu <naokitsu@tutanota.com>
2022-09-28 15:01:17 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Fantastic Project Template
+CMake project template
+
+That's it
---
+++ cmake/signs.cmake
@@ -0,0 +1,4 @@
+set(BigArrow        "[1;2m[1;36m==>[0m[0m")
+set(Block           "[1;2m[1;36m[1;34m::[0m[1;36m[0m[0m")
+set(SmallArrow      "[1;2m[1;36m[1;34m  ->[0m[1;36m[0m[0m")
+set(ErrorSmallArrow "[2;31m[1;31m  ->[0m[2;31m[0m")
